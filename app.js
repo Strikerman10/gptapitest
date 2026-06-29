57f64d8ecf46c1301c173754529f50fcc2a28808
Strikerman10 <barney.willis2@gmail.com>
2026-06-29 21:17:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -170,16 +170,10 @@ function handleUnauthorized() {
   // ==========================
   // INPUT AUTO RESIZE
   // ==========================
-const expandBtn = document.getElementById("expandBtn");
-const COLLAPSED_HEIGHT = 42;
-const EXPANDED_HEIGHT = 102;
-let isExpanded = false;
-  
   function autoResize() {
-    if (isExpanded) return;
     inputEl.style.height = "auto";
-    inputEl.style.height = Math.min(inputEl.scrollHeight, COLLAPSED_HEIGHT) + "px";
-}
+    inputEl.style.height = Math.min(inputEl.scrollHeight, 200) + "px";
+  }
   inputEl.addEventListener("input", autoResize);
   autoResize();
 
